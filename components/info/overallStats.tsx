4817079b82a90c974cd1859f45e494c2ad7bb265
Grigore-Gabriel Trifan <trifangaby71@protonmail.com>
2023-01-31 22:39:36 +0200

    revamp stats look

--- components/info/overallStats.tsx
+++ components/info/overallStats.tsx
@@ -1,34 +1,43 @@
-import { CircleWavyCheck, CurrencyCircleDollar, Users } from 'phosphor-react'
+import { CircleWavyCheck, CurrencyCircleDollar, CurrencyEth, Users } from 'phosphor-react'
 import React from 'react'
 
 const OverallStats = () => {
     return (
-        <div className="stats shadow-lg shadow-black/20 bg-base-100">
+        <div className="sm:flex justify-evenly lg:justify-center">
 
-            <div className="stat">
-                <div className="stat-figure text-primary">
-                    <Users size={32} />
+            <div className="stats shadow-lg  bg-base-100 stats-vertical xl:stats-horizontal m-2">
+                <div className="stat">
+                    <div className="stat-figure text-primary">
+                        <CurrencyCircleDollar size={32} />
+                    </div>
+                    <div className="stat-title">Amount Contributed</div>
+                    <div className="stat-value text-primary">2.6M</div>
                 </div>
-                <div className="stat-value text-primary">25.6K</div>
-                <div className="stat-title">Total Donors</div>
-            </div>
-
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <CurrencyCircleDollar size={32} />
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <CurrencyEth size={32} />
+                    </div>
+                    <div className="stat-title">Amount Contributed</div>
+                    <div className="stat-value text-secondary">1066</div>
                 </div>
-                <div className="stat-title">Amount Contributed</div>
-                <div className="stat-value text-secondary">2.6M</div>
             </div>
+            <div className="stats bg-base-100 stats-vertical xl:stats-horizontal m-2">
 
-            <div className="stat">
-                <div className="stat-figure text-secondary">
-                    <CircleWavyCheck size={32} />
+                <div className="stat">
+                    <div className="stat-figure text-primary">
+                        <Users size={32} />
+                    </div>
+                    <div className="stat-value text-primary">25.6K</div>
+                    <div className="stat-title">Total Donors</div>
                 </div>
-                <div className="stat-value">12K</div>
-                <div className="stat-title">Unique donors</div>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <CircleWavyCheck size={32} />
+                    </div>
+                    <div className="stat-value">12K</div>
+                    <div className="stat-title">Unique donors</div>
+                </div>
             </div>
-
         </div>
     )
 }
