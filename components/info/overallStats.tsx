c25f2aff26681694cac8733ddf73ae0b803c96e2
Grigore-Gabriel Trifan <trifangaby71@protonmail.com>
2023-01-31 23:44:51 +0200

    tweaks stats

--- components/info/overallStats.tsx
+++ components/info/overallStats.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const OverallStats = () => {
     return (
-        <div className="sm:flex justify-evenly lg:justify-center">
+        <div className="sm:flex justify-evenly lg:justify-center mx-auto" style={{ maxWidth: "90rem" }}>
 
-            <div className="stats shadow-lg  bg-base-100 stats-vertical xl:stats-horizontal m-2">
+            <div className="stats shadow-lg  bg-base-100 stats-vertical xl:stats-horizontal m-2 w-10/12 max-w-96">
                 <div className="stat">
                     <div className="stat-figure text-primary">
                         <CurrencyCircleDollar size={32} />
@@ -21,7 +21,7 @@ const OverallStats = () => {
                     <div className="stat-value text-secondary">1066</div>
                 </div>
             </div>
-            <div className="stats bg-base-100 stats-vertical xl:stats-horizontal m-2">
+            <div className="stats bg-base-100 stats-vertical xl:stats-horizontal m-2 w-10/12 max-w-96">
 
                 <div className="stat">
                     <div className="stat-figure text-primary">
